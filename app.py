e58ca7b7eafb8be7194b162f2c2c45318b2ef036
nikhilcusc <nikhilc@usc.edu>
2026-07-29 15:59:51 -0700

    update app.py
    Added default port number.

--- app.py
+++ app.py
@@ -299,4 +299,4 @@ with gr.Blocks(title="Local Brain Age Inference App") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 10000)))
